72259f503ea3db4f9d54da59a32452c11e438177
Jordan Shartar <jshartar6@gatech.edu>
2017-06-01 12:48:19 -0400

    fixed ant lab, added gtid

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -70,7 +70,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("jordan shartar");
-                  toPrint += p4.toString("jshartar6");
+                  toPrint += p4.toString("903131050");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
